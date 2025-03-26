781a0b7487f3255154352e98c1dd3a5543517f4c
maikkatko <maikkatko@gmail.com>
2025-03-25 21:10:03 -0400

    Initial commit

---
+++ src/main/java/io/github/maikkatko/crm/CrmIntegrationPipelineApplication.java
@@ -0,0 +1,13 @@
+package io.github.maikkatko.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmIntegrationPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmIntegrationPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/maikkatko/crm/CrmIntegrationPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maikkatko.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmIntegrationPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
